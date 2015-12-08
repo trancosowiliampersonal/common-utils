9180a1787d8e9e6990c5d052dd5fb93482de6217
Wiliam Trancoso <wiliam.trancoso@avistanet.com.br>
2015-12-08 18:16:23 -0200

    [master] [wiliam trancoso]   \_> criada anotacao responsavel pelo parse

--- src/com/dalcim/annotations/BindProperty.java
+++ src/com/dalcim/annotations/BindProperty.java
@@ -1,5 +1,12 @@
 package com.dalcim.annotations;
 
-public @interface BindProperty {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target( value = ElementType.FIELD )
+@Retention( value = RetentionPolicy.RUNTIME )
+public @interface BindProperty {
+	String value() default "";
 }
