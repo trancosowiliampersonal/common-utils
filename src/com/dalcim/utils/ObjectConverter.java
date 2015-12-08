175a1f07075f96241e28a1afd8cea39c7f4a30a7
Wiliam Trancoso <wiliam.trancoso@avistanet.com.br>
2015-12-08 18:16:49 -0200

    [master] [wiliam trancoso]   \_> implementado o parse

--- src/com/dalcim/utils/ObjectConverter.java
+++ src/com/dalcim/utils/ObjectConverter.java
@@ -1,5 +1,47 @@
 package com.dalcim.utils;
 
-public class ObjectConverter {
+import java.lang.reflect.Field;
 
+import com.dalcim.annotations.BindProperty;
+
+public class ObjectConverter {
+	
+	public static <T> T setFields(Object de, Class<T> para) throws Exception {
+		
+		Class<?> fieldsAnnotations = de.getClass();
+		T objRetorno = para.newInstance();
+		
+		BindProperty bp;
+		String nameProperty = null;
+		
+		try {
+			for (Field f : fieldsAnnotations.getDeclaredFields()) {
+				
+				bp = f.getAnnotation(BindProperty.class);
+				nameProperty = !bp.value().isEmpty() ? bp.value() : f.getName();
+				
+				if (bp != null) {
+
+					
+					Field ft;
+					
+					ft = para.getDeclaredField( nameProperty );
+						
+					f.setAccessible(true);
+					ft.setAccessible(true);
+					
+					ft.set(objRetorno, f.get(de));
+					
+					ft.setAccessible(false);
+					f.setAccessible(false);
+				}
+				
+			}
+		}catch(Exception e){
+			
+		}
+		
+		return objRetorno;
+	}
+	
 }
